e705543c37663384f78a8ccf05706ef6423a6737
mvkump <mvkump@utu.fi>
2021-02-24 17:50:26 +0200

    small cleanupsa and author link

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
 import SignUpForm from "../SignUpForm/SignUpForm";
-import "./App.css";
 import "../../assets/main.css";
+import "./App.css";
 
 const App: FC = () => {
   return (
     <div className="App">
       <SignUpForm />
+      <div id="credentials">
+        <a href="https://www.linkedin.com/in/matias-kumpulainen-27b4421b9/">
+          Author
+        </a>
+      </div>
     </div>
   );
 };
